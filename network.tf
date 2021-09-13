0c368a6b30384fa4c7cd1065d0e038654a8c0f63
zakko13 <varsallll@gmail.com>
2021-09-13 10:00:05 +0300

    Change CIDR block

--- network.tf
+++ network.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "public_subnet2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.0.1/24"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = local.availability_zones[1]
   map_public_ip_on_launch = true
 
